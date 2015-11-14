6c383b636b9b793d51963ab3176ad486563fc509
Lauren <lauren.e.coombe@gmail.com>
2015-11-14 11:59:07 -0800

    Added link to index.html in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
    console.log("Got a GET request for the homepage");
-   res.send('Hello GET');
+   res.sendFile(__dirname + "/public/index.html");
 })
 
 
